adc9bc64751982667dd4e90f0eecba6a4237487d
Rohit Krishnan <rohit.k.kesavan@gmail.com>
2022-12-24 16:20:48 +0530

    test: add vote tests

--- src/test/java/in/alqaholic/VotingAPI/VotingServiceTests.java
+++ src/test/java/in/alqaholic/VotingAPI/VotingServiceTests.java
@@ -2,15 +2,20 @@ package in.alqaholic.VotingAPI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.Collections;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import in.alqaholic.VotingAPI.exceptions.CandidateAlreadyExists;
+import in.alqaholic.VotingAPI.exceptions.CandidateDoesNotExist;
 import in.alqaholic.VotingAPI.exceptions.NoCandidatesEntered;
 import in.alqaholic.VotingAPI.repository.VoteRepository;
 import in.alqaholic.VotingAPI.services.VoteService;
@@ -56,4 +61,41 @@ public class VotingServiceTests {
         when(voteRepository.getWinner()).thenReturn(null);
         assertThrows(NoCandidatesEntered.class, () -> voteService.getWinner());
     }
+
+    @Test
+    void listVotes() {
+        var voteList = Collections.singletonMap("test", 1);
+        when(voteRepository.getVoteList()).thenReturn(voteList);
+        assertTrue(() -> voteService.getVoteList().entrySet().stream()
+                .allMatch(entry -> entry.getValue().equals(voteList.get(entry.getKey()))));
+    }
+
+    @Test
+    void getVotes() {
+        when(voteRepository.getVotes("test1")).thenReturn(2);
+        when(voteRepository.exists("test1")).thenReturn(true);
+        when(voteRepository.getVotes("test2")).thenReturn(4);
+        when(voteRepository.exists("test2")).thenReturn(true);
+        assertEquals(voteService.getVotes("test1"), 2);
+        assertEquals(voteService.getVotes("test2"), 4);
+    }
+
+    @Test
+    void getVotesForNonExistantCandidate() {
+        when(voteRepository.exists("test1")).thenReturn(false);
+        when(voteRepository.getVotes("test2")).thenReturn(4);
+        when(voteRepository.exists("test2")).thenReturn(true);
+        assertThrows(CandidateDoesNotExist.class, () -> voteService.getVotes("test1"));
+        assertEquals(voteService.getVotes("test2"), 4);
+    }
+
+    @Test
+    void castVote() {
+        String name = "test";
+        voteService.castVote(name);
+        voteService.castVote(name);
+        voteService.castVote(name);
+        verify(voteRepository, times(3)).castVote(name);
+    }
+
 }
